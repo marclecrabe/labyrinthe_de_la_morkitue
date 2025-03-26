ec128198f38a36c5783df14ddd24fa2cbec34605
DUFEY MARC <marc.df@localhost.localdomain>
2025-03-26 11:45:05 +0100

    MAMA GUEVO

--- laby.py
+++ laby.py
@@ -1,1 +1,37 @@
+from maqueen import *
+from microbit import *
+import utime
 
+# Constantes
+blanco = 0
+negro = 1
+
+# Variable globale
+Init = True
+final = False
+visto = False
+
+def mirar():
+    return [line_sensor(LineSensor.L1),line_sensor(LineSensor.M),line_sensor(LineSensor.R1)]
+
+def pared():
+    if mirar() != [blanco,blanco,blanco]:
+        visto = True
+
+def jirar():
+    if visto = True:
+         motor_stop()
+         motor_run(Motor.LEFT,speed						#MAMA GUEVO on est là biloute, cara de verga
+
+def 
+while True:
+    if Init:
+        # Vitesse maximale des moteurs
+        speed:int = 40   
+        speed_slow:int = 12 
+        Init = False
+        music.pitch(880,10)
+        
+    utime.sleep(10)
+    
+    pared()
