da7e42785dadc56ca6408c01ed72b27262115a74
REYES ALEXANDRE <alexandre.rs2@localhost.localdomain>
2025-04-02 11:35:21 +0200

    code 2

--- laby.py
+++ laby.py
@@ -16,23 +16,25 @@ visto = False
 
 def look():
     return [line_sensor(LineSensor.L1),line_sensor(LineSensor.M),line_sensor(LineSensor.R1)]
+    print (" estoy mirando uevon")
 
 def mur_attention():
+    print (" estoy ")
     if look() != [blanco,blanco,blanco]:
         visto = True
 
 def tourner():
     if visto == True:
          motor_stop()
-         #motor_run(Motor.LEFT,speed, FORWARD)
+         motor_run(Motor.LEFT,speed, FORWARD)
          
 def En_avant_vers_le_bout_du_monde():
     look()
     mur_attention()
     motor_run(Motor.LEFT,speed,FORWARD)
     motor_run(Motor.RIGHT,speed,FORWARD)
-         
-#MAMA GUEVO on est là biloute, cara de verga
+     
+#MAMA GUEVO on est là biloute
 
 while True:
     if Init:
@@ -40,6 +42,6 @@ while True:
         speed:int = 40   
         speed_slow:int = 12 
         Init = False
-    
+
     En_avant_vers_le_bout_du_monde()
-    utime.sleep(10)
+    sleep(10)
